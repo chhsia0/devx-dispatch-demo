590e688ed12bcd20c3494633b7d51d953e35c56d
Chun-Hung Hsiao <chhsiao@mesosphere.io>
2019-11-08 10:36:48 -0800

    Update main.go

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world!\n")
 	fmt.Fprintf(w, "Version: %s\n", Version)
 	fmt.Fprintf(w, "Build time: %s\n", Buildtime)
-	fmt.Fprintf(w, "Have you heard of Dispatch?\n")
+	fmt.Fprintf(w, "Yes Dispatch is cool!\n")
 }
 
 // [END all]
